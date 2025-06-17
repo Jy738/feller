3798843cec27b017ca935099560ed515349e8268
shoebill <84299531+Jy738@users.noreply.github.com>
2025-06-17 14:19:05 -0500

    Update index.js

--- index.js
+++ index.js
@@ -22,10 +22,10 @@ const Global = {
       if(options.iterations == undefined)options.iterations = 1000;
       if(options.samples == undefined)options.samples = 5;
       let samples = [];
-      for(let currentSample = 0; currentSample < options.samples; currentSample++;){
+      for(let currentSample = 0; currentSample < options.samples; currentSample++){
          let newNet = net.nodeFull;
          samples = [...samples,newNet];
-         for(let nodeQueue = 0; nodeQueue < newNet.hiddenLayers.length; nodeQueue++;){
+         for(let nodeQueue = 0; nodeQueue < newNet.hiddenLayers.length; nodeQueue++){
             let currentNode = newNet[nodeQueue];
             currentNoode
           }
